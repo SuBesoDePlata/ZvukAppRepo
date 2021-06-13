af703809af8c03204beb96b70ca7fb99aaa95b1f
SuBesoDePlata <subesodeplata@gmail.com>
2021-06-13 16:43:17 +0300

    Unit tests added

---
+++ UnitTests/AddEditTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests;
+
+namespace AddEditTest
+{
+    [TestClass()]
+    public class LoginTestClass
+    {
+        [TestMethod()]
+        public void LoginTest()
+        {
+            var addedit1 = TestMethod("Иван", "Иванович", "Иванов", DateTime.Parse("1980-12-12"), "123321", 1, "321123", 0);
+            var addedit2 = TestMethod("", "Иванович", "Иванов", DateTime.Parse("1980-12-12"), "ale", 1, "ale", 0);
+            Assert.AreEqual(1, addedit1);
+            Assert.AreEqual(0, addedit2);
+        }
+
+
+        public static int TestMethod(string firstName, string lastName, string patronymic, DateTime dateOfBirth, string login, int userType, string password, int userId)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(firstName))
+                errors.AppendLine("Укажите имя");
+            if (string.IsNullOrWhiteSpace(lastName))
+                errors.AppendLine("Укажите фамилию");
+            if (string.IsNullOrWhiteSpace(patronymic))
+                errors.AppendLine("Укажите отчество");
+            if (dateOfBirth == null)
+                errors.AppendLine("Укажите дату рождения");
+            if (userType < 0)
+                errors.AppendLine("Укажите тип пользователя");
+            if (string.IsNullOrWhiteSpace(login))
+                errors.AppendLine("Укажите логин");
+            if (string.IsNullOrWhiteSpace(password))
+                errors.AppendLine("Укажите пароль");
+
+            if (errors.Length > 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+
+}
---
+++ UnitTests/LoginTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests;
+
+namespace LoginTest
+{
+    [TestClass()]
+    public class LoginTestClass
+    {
+        [TestMethod()]
+        public void LoginTest()
+        {
+            var auth1 = TestMethod("hohl", "hohl");
+            var auth2 = TestMethod("1", "1");
+            Assert.AreEqual(1, auth1);
+            Assert.AreEqual(0, auth2);
+        }
+        public static int TestMethod(string login, string password)
+        {
+            try
+            {
+                using (ZvukDBEntities db = new ZvukDBEntities())
+                {
+                    foreach (var user in ZvukDBEntities.getContext().Users)
+                    {
+                        if (login == user.Login && password == user.Password)
+                        {
+                            if (user.TypeId == 1)
+                            {
+                                return 1;
+                            }
+                            else
+                            if (user.TypeId == 2)
+                            {
+                                return 2;
+
+                            }
+                        }
+
+
+                    }
+                    return 0;
+                }
+
+            }
+            catch
+            {
+                return 0;
+            }
+
+        }
+    }
+
+}
